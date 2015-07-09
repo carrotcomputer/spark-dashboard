2709215188444c612515ea3b0b03d507073463ef
Giovanni Lenguito <giovanni@sparkrevolutions.com>
2015-07-09 10:57:26 +0100

    Add Project Final - Unnecessary files removed (traffic-light) - 'add-project' template contains all fields - data stores in local storage - install of local-storage

--- app/models/project.js
+++ app/models/project.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 var Project = DS.Model.extend({
   title: DS.attr('string'),
   milestone: DS.attr('string'),
+  deadline: DS.attr('string'),
+  userName: DS.attr('string'),
+  lightStatus: DS.attr('string')
 });
 
 export default Project;
