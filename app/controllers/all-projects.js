d32ba00aba8780f71d91197abb42c935847d4c06
Giovanni Lenguito <giovanni@sparkrevolutions.com>
2015-07-09 15:44:14 +0100

    Delete project - implements delete project function - includes small UI updates

--- app/controllers/all-projects.js
+++ app/controllers/all-projects.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    deleteCard: function(proj){
+      
+      if(confirm("You are about to remove this project (" +  proj.get('title') + ")?")){
+        proj.deleteRecord();
+        proj.save();
+      }
+    }
+  }
 });
