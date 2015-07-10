ca7a8e17ee37a7b5a9975d26333e2f7c68f25c89
Giovanni Lenguito <giovanni@sparkrevolutions.com>
2015-07-10 17:19:12 +0100

    Improvement - updates error checking when adding new user - new transition from new user to all users view

--- app/controllers/new-user.js
+++ app/controllers/new-user.js
@@ -6,20 +6,22 @@ export default Ember.Controller.extend({
     addUser: function(){
       var uName = this.get('name');
       
-      if(!uName){
-        this.set('isError', true);
+      if(uName){
+        if(uName.trim().isEmpty()){
+          this.set('isError',false);
+        }else{
+          var user = this.store.createRecord('user', {
+              userName: uName
+          });
+      
+          user.save();
+      
+          this.set('name', '');
+          this.transitionTo('all-users');
+        }
       }else{
-        this.set('isError',false);
-        
-        var user = this.store.createRecord('user', {
-            userName: uName
-        });
-      
-        user.save();
-      
-        this.set('name', '');
-        this.transitionTo('all-projects');
-    }
+        this.set('isError', true);
+      }
     }
   }
 });
